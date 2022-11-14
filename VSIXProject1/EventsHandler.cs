e11bb5ead2325e744bfc1b20192e60e4f2b75746
georgii-borovinskikh-sonarsource <georgii.borovinskikh@sonarsource.com>
2022-11-14 13:17:25 +0100

    Add project files.

---
+++ VSIXProject1/EventsHandler.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace VSIXProject1
+{
+    internal class EventsHandler : 
+        IVsSolutionEvents3,
+        IVsSolutionEvents4,
+        IVsSolutionEvents5,
+        IVsSolutionEvents6,
+        IVsSolutionEvents7,
+        IVsSolutionEvents8
+    {
+        private readonly IVsOutputWindowPane vsOutputWindowPane;
+
+        public EventsHandler(IVsOutputWindowPane vsOutputWindowPane)
+        {
+            this.vsOutputWindowPane = vsOutputWindowPane;
+        }
+
+        int IVsSolutionEvents.OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            PrintEventMessage("Project opened");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+        {
+            PrintEventMessage("Project closing queried");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+        {
+            PrintEventMessage("Project about to close");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+        {
+            PrintEventMessage("Project loaded");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+        {
+            PrintEventMessage("Project unloading queried");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+        {
+            PrintEventMessage("Project about to unload");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+        {
+            PrintEventMessage("Solution opened");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+        {
+            PrintEventMessage("Solution closing queried");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnBeforeCloseSolution(object pUnkReserved)
+        {
+            PrintEventMessage("Solution about to close");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnAfterCloseSolution(object pUnkReserved)
+        {
+            PrintEventMessage("Solution closed");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnAfterMergeSolution(object pUnkReserved)
+        {
+            PrintEventMessage("Solution merged");
+            return 0;
+        }
+
+        public int OnBeforeOpeningChildren(IVsHierarchy pHierarchy)
+        {
+            PrintEventMessage("About to open children");
+            return 0;
+        }
+
+        public int OnAfterOpeningChildren(IVsHierarchy pHierarchy)
+        {
+            PrintEventMessage("Opened children");
+            return 0;
+        }
+
+        public int OnBeforeClosingChildren(IVsHierarchy pHierarchy)
+        {
+            PrintEventMessage("Children about to close");
+            return 0;
+        }
+
+        public int OnAfterClosingChildren(IVsHierarchy pHierarchy)
+        {
+            PrintEventMessage("Children closed");
+            return 0;
+        }
+
+        int IVsSolutionEvents3.OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            PrintEventMessage("Project opened");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+        {
+            PrintEventMessage("Project closing queried");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+        {
+            PrintEventMessage("Project about to close");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+        {
+            PrintEventMessage("Project loaded");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+        {
+            PrintEventMessage("Project unloading queried");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+        {
+            PrintEventMessage("Project about to unload");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+        {
+            PrintEventMessage("Solution opened");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+        {
+            PrintEventMessage("Solution closing queried");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnBeforeCloseSolution(object pUnkReserved)
+        {
+            PrintEventMessage("Solution about to close");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnAfterCloseSolution(object pUnkReserved)
+        {
+            PrintEventMessage("Solution closed");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnAfterMergeSolution(object pUnkReserved)
+        {
+            PrintEventMessage("Solution merged");
+            return 0;
+        }
+
+        int IVsSolutionEvents2.OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            PrintEventMessage("Project opened");
+            return 0;
+        }
+
+        int IVsSolutionEvents.OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+        {
+            PrintEventMessage("Project closing queried");
+            return 0;
+        }
+
+        int IVsSolutionEvents.OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+        {
+            PrintEventMessage("Project about to close");
+            return 0;
+        }
+
+        int IVsSolutionEvents.OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+        {
+            PrintEventMessage("Project loaded");
+            return 0;
+        }
+
+        int IVsSolutionEvents.OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+        {
+            PrintEventMessage("Project unloading queried");
+            return 0;
+        }
+
+        int IVsSolutionEvents.OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+        {
+            PrintEventMessage("Project about to unload");
+            return 0;
+        }
+
+        int IVsSolutionEvents.OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+        {
+            PrintEventMessage("Solution opened");
+            return 0;
+        }
+
+        int IVsSolutionEvents.OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+        {
+            PrintEventMessage("Solution closing queried");
+            return 0;
+        }
+
+        int IVsSolutionEvents.OnBeforeCloseSolution(object pUnkReserved)
+        {
+            PrintEventMessage("Solution about to close");
+            return 0;
+        }
+
+        int IVsSolutionEvents.OnAfterCloseSolution(object pUnkReserved)
+        {
+            PrintEventMessage("Solution closed");
+            return 0;
+        }
+
+        public int OnAfterRenameProject(IVsHierarchy pHierarchy)
+        {
+            PrintEventMessage("Project renamed");
+            return 0;
+        }
+
+        public int OnQueryChangeProjectParent(IVsHierarchy pHierarchy, IVsHierarchy pNewParentHier, ref int pfCancel)
+        {
+            PrintEventMessage("Project parent change queried");
+            return 0;
+        }
+
+        public int OnAfterChangeProjectParent(IVsHierarchy pHierarchy)
+        {
+            PrintEventMessage("Project parent changed");
+            return 0;
+        }
+
+        public int OnAfterAsynchOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            PrintEventMessage("Project opened asynchronously");
+            return 0;
+        }
+
+        public void OnBeforeOpenProject(ref Guid guidProjectID, ref Guid guidProjectType, string pszFileName)
+        {
+            PrintEventMessage("Project about to open");
+        }
+
+        public int OnBeforeProjectRegisteredInRunningDocumentTable(Guid projectID, string projectFullPath)
+        {
+            PrintEventMessage("Project about to be registered");
+            return 0;
+        }
+
+        public int OnAfterProjectRegisteredInRunningDocumentTable(Guid projectID, string projectFullPath, uint docCookie)
+        {
+            PrintEventMessage("Project registered");
+            return 0;
+        }
+
+        public void OnAfterOpenFolder(string folderPath)
+        {
+            PrintEventMessage("Folder opened");
+        }
+
+        public void OnBeforeCloseFolder(string folderPath)
+        {
+            PrintEventMessage("Folder about to be closed");
+        }
+
+        public void OnQueryCloseFolder(string folderPath, ref int pfCancel)
+        {
+            PrintEventMessage("Folder closing queried");
+        }
+
+        public void OnAfterCloseFolder(string folderPath)
+        {
+            PrintEventMessage("Folder closed");
+        }
+
+        public void OnAfterLoadAllDeferredProjects()
+        {
+            PrintEventMessage("Deferred projects loaded");
+        }
+
+        public void OnAfterRenameSolution(string oldName, string newName)
+        {
+            PrintEventMessage("Solution renamed");
+        }
+
+        private void PrintEventMessage(string message)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            vsOutputWindowPane.OutputStringThreadSafe(message + "\r\n");
+        }
+    }
+}
---
+++ VSIXProject1/VSIXProject1Package.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell.Events;
+using Task = System.Threading.Tasks.Task;
+using static Microsoft.VisualStudio.VSConstants;
+using Microsoft;
+
+namespace VSIXProject1
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(VSIXProject1Package.PackageGuidString)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class VSIXProject1Package : AsyncPackage, IDisposable
+    {
+        private IVsOutputWindowPane vsOutputWindowPane;
+
+        /// <summary>
+        /// VSIXProject1Package GUID string.
+        /// </summary>
+        public const string PackageGuidString = "4957da3a-6252-4bbf-acc1-17c1c80577f9";
+
+        private uint cookie = VSConstants.VSCOOKIE_NIL;
+        private IVsSolution solution;
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            vsOutputWindowPane = (IVsOutputWindowPane) await GetServiceAsync(typeof(SVsGeneralOutputWindowPane));
+            Assumes.Present(vsOutputWindowPane);
+            solution = (IVsSolution) await GetServiceAsync(typeof(SVsSolution));
+            Assumes.Present(solution);
+            solution.AdviseSolutionEvents(new EventsHandler(vsOutputWindowPane), out cookie);
+
+            //SolutionEvents.OnAfterRenameProject += (sender, args) => args.Hierarchy.;
+            //SolutionEvents.OnAfterAsynchOpenProject += (sender, args) => PrintEventMessage("Opened project");
+            //SolutionEvents.OnAfterChangeProjectParent += (sender, args) => PrintEventMessage("Moved project");
+            //SolutionEvents.OnAfterCloseFolder += (sender, args) => PrintEventMessage("Folder closed");
+            //SolutionEvents.OnAfterCloseSolution += (sender, args) => PrintEventMessage("Solution closed");
+            //SolutionEvents.OnAfterClosingChildren += (sender, args) => PrintEventMessage("Nested projects closed");
+            //SolutionEvents.OnAfterLoadAllDeferredProjects += (sender, args) => PrintEventMessage("Deferred projects loaded"); // not supported in vs22
+            //SolutionEvents.OnAfterLoadProject += (sender, args) => PrintEventMessage("Project loaded");
+            //SolutionEvents.OnAfterMergeSolution += (sender, args) => PrintEventMessage("Merged solution");
+            //SolutionEvents.OnAfterOpenFolder += (sender, args) => PrintEventMessage("Folder opened");
+            //SolutionEvents.OnAfterOpenProject += (sender, args) => PrintEventMessage("Project opened");
+            //SolutionEvents.OnAfterOpenSolution += (sender, args) => PrintEventMessage("Solution opened");
+            //SolutionEvents.OnAfterOpeningChildren += (sender, args) => PrintEventMessage("Children opened");
+
+
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            if (cookie != VSCOOKIE_NIL)
+            {
+                solution.UnadviseSolutionEvents(cookie);
+                solution = null;
+                cookie = VSCOOKIE_NIL;
+            }
+        }
+    }
+}
